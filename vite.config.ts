ae72060e5759ac6ee914d4be027a212adc417603
Mohamad-haghi <mohamadhaghi64@gmail.com>
2026-09-08 17:16:29 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 export default defineConfig({
+  base: "/skate-shop-new/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
